01a134f950c06b338a6fcd83960f2f87ff127686
Krzysztof Prawdzik <morphem3000@gmail.com>
2020-10-01 11:05:50 +0200

    Initial Commit for Spring Boot Reddit Clone

---
+++ src/test/java/eu/blinkneuron/springit/SpringitApplicationTests.java
@@ -0,0 +1,11 @@
+package eu.blinkneuron.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
