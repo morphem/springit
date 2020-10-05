51cd111fc808654781ff9d8732a699d21fc46065
Krzysztof Prawdzik <morphem3000@gmail.com>
2020-10-05 14:11:06 +0200

    Adding a home controller for demo purposes

---
+++ src/main/java/eu/blinkneuron/springit/controller/MyHomeController.java
@@ -0,0 +1,17 @@
+package eu.blinkneuron.springit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyHomeController {
+
+  @GetMapping("/")
+  public String home(Model model, HttpServletRequest request) {
+    model.addAttribute("message", "Hello World!") ;
+    return "index";
+  }
+}
